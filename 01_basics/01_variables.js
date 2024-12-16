bf760ee0e5965b2ba5c35f4201e336b2da2751c0
pooja01-sahu <poojasahu11296@gmail.com>
2024-12-16 10:40:55 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail =  "poojasahu11296@gmailcom"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2 // not allowed
+accountEmail = "pooji@gmail.com"
+accountPassword = "34542"
+accountCity = "Indore"
+let accountState;
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
