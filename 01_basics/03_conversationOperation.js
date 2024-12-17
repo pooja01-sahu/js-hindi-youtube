c8fd6e77eb6e456edf705e00499e0186352f0a3a
pooja01-sahu <poojasahu11296@gmail.com>
2024-12-17 09:20:36 +0000

    conversation of dataTypes

---
+++ 01_basics/03_conversationOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+// console.log(typeof score)
+
+let valueNumber = Number(score)
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+//"33" => 33
+// "33qbc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "pooja" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"pooja" => true
+
+let someNumber = 33 
+ let stringNumber = String(someNumber)
+ console.log(typeof stringNumber)
